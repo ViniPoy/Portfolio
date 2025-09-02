23534ae9abb54a1017210b546ce83e4fd0d13941
ViniPoy <vinipoy@gmail.com>
2025-09-02 16:18:03 +0200

    Ajout de scripts pour générer la galerie dynamiquement.

---
+++ scripts/appel-API.js
@@ -0,0 +1,88 @@
+async function getProject() {
+    const responseProject = await fetch("http://localhost:4000/api/portfolio");
+    const project = await responseProject.json();
+    return project;
+};
+
+async function postProject(data) {
+    try {
+        const response = await fetch("http://localhost:4000/api/portfolio", {
+            method: "POST",
+            headers: { "Authorization" : `Bearer ${token}` },
+            body: data
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error("Erreur lors de l'envoi :", response.status);
+            return null;
+        }
+    } catch (error) {
+        console.error("Erreur réseau :", error);
+        return null;
+    }
+}
+
+async function putProject(data, id) {
+    try {
+        const response = await fetch(`http://localhost:4000/api/portfolio/${id}`, {
+            method: "PUT",
+            headers: { 
+                "Authorization" : `Bearer ${token}`,
+                "Content-Type" : "application/json"
+            },
+            body: data
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error("Erreur lors de l'envoi :", response.status);
+            return null;
+        }
+    } catch (error) {
+        console.error("Erreur réseau :", error);
+        return null;
+    }
+}
+
+async function deleteProject(id) {
+    try {
+        const response = await fetch(`http://localhost:4000/api/portfolio/${id}`, {
+            method: "DELETE",
+            headers: { 
+                "Authorization" : `Bearer ${token}`,
+                "Content-Type" : "application/json"
+            }
+        });
+        return { success: response.ok, status: response.status };
+    } catch (error) {
+        console.error("Erreur réseau :", error);
+        return false;
+    }
+}
+
+
+
+async function postAdmin(email, password) {
+    try{
+        const response = await fetch("http://localhost:4000/api/auth/login", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({ email, password })
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error("Erreur lors de l'envoie :", response.status);
+            return null;
+        }
+    } catch (error) {
+        showError(error.message);
+    }
+}
+
+
+
+async function getCategories() {
+    const responseCategories = await fetch("http://localhost:4000/")
+}
---
+++ scripts/portfolio.js
@@ -0,0 +1,92 @@
+const divFilters = document.querySelector(".portfolio__filters");
+const divGallery = document.querySelector(".portfolio__gallery");
+
+let projects;
+
+getProject()
+    .then((data) => {
+        projects = data;
+        generateFilters();
+        generateGallery(projects);
+    })
+    .catch(error => console.error("Erreur lors de la récupération des données :", error));
+
+function generateFilters() {
+    const allButton = document.createElement("button");
+    allButton.textContent = "Tous";
+    allButton.classList.add("portfolio__filters--button", "portfolio__filters--button-active");
+    allButton.addEventListener("click", () => {
+        document.querySelectorAll(".portfolio__filters--button").forEach(btn => btn.classList.remove("portfolio__filters--button-active"));
+        allButton.classList.add("portfolio__filters--button-active");
+        generateGallery(projects);
+    });
+    divFilters.appendChild(allButton);
+
+    const categories = [...new Set(projects.map(project => project.category))];
+    categories.forEach(category => {
+        const button = document.createElement("button");
+        button.textContent = category;
+        button.classList.add("portfolio__filters--button");
+        button.addEventListener("click", () => {
+            document.querySelectorAll(".portfolio__filters--button").forEach(btn => btn.classList.remove("portfolio__filters--button-active"));
+            button.classList.add("portfolio__filters--button-active");
+            generateGallery(projects.filter(project => project.category === category));
+        });
+        divFilters.appendChild(button);
+    });
+}
+
+function generateGallery(projectToShow) {
+    divGallery.innerHTML = "";
+    projectToShow.forEach((card) => {
+        generateProject(card);
+    });
+}
+
+function generateProject(card) {
+    const cardElement = document.createElement("div");
+    cardElement.id = `project-${card.id}`;
+    cardElement.classList.add("portfolio__card");
+    
+    const imageElement = document.createElement("img");
+    imageElement.src = card.image;
+    imageElement.alt = card.alt;
+    imageElement.classList.add("portfolio__card--image");
+
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = card.title;
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = card.description;
+    descriptionElement.classList.add("portfolio__card--description");
+
+    const skillsElement = document.createElement("p");
+    skillsElement.textContent = card.skills.join("\n");
+    skillsElement.style.whiteSpace = "pre-line";
+    skillsElement.classList.add("portfolio__card--skills");
+
+    const linksContainer = document.createElement("div");
+    linksContainer.classList.add("portfolio__card--links-container");
+    const links = card.link;
+    if (links.github) {
+        const githubLink = document.createElement("a");
+        githubLink.href = card.link.github;
+        githubLink.textContent = "GitHub";
+        githubLink.target = "_blank";
+        githubLink.rel = "noopener noreferrer";
+        githubLink.classList.add("portfolio__card--link");
+        linksContainer.appendChild(githubLink);
+    }
+    if (links.site) {
+        const siteLink = document.createElement("a");
+        siteLink.href = card.link.site;
+        siteLink.textContent = "Visiter le site";
+        siteLink.target = "_blank";
+        siteLink.rel = "noopener noreferrer";
+        siteLink.classList.add("portfolio__card--link");
+        linksContainer.appendChild(siteLink);
+    }
+
+    cardElement.append(imageElement, titleElement, descriptionElement, skillsElement, linksContainer);
+    divGallery.appendChild(cardElement);
+}
