38be7ae634e70f80d654a6da84fe262bf51f6861
ViniPoy <vinipoy@gmail.com>
2025-09-05 14:38:45 +0200

    modif des fetch pour accèder au backend en ligne sur render

--- scripts/appel-API.js
+++ scripts/appel-API.js
@@ -1,12 +1,12 @@
 async function getProject() {
-    const responseProject = await fetch("http://localhost:4000/api/portfolio");
+    const responseProject = await fetch("https://portfolio-backend-4kbf.onrender.com/api/portfolio");
     const project = await responseProject.json();
     return project;
 };
 
 async function postProject(data) {
     try {
-        const response = await fetch("http://localhost:4000/api/portfolio", {
+        const response = await fetch("https://portfolio-backend-4kbf.onrender.com/api/portfolio", {
             method: "POST",
             headers: { "Authorization" : `Bearer ${token}` },
             body: data
@@ -25,7 +25,7 @@ async function postProject(data) {
 
 async function putProject(data, id) {
     try {
-        const response = await fetch(`http://localhost:4000/api/portfolio/${id}`, {
+        const response = await fetch(`https://portfolio-backend-4kbf.onrender.com/api/portfolio/${id}`, {
             method: "PUT",
             headers: { 
                 "Authorization" : `Bearer ${token}`,
@@ -47,7 +47,7 @@ async function putProject(data, id) {
 
 async function deleteProject(id) {
     try {
-        const response = await fetch(`http://localhost:4000/api/portfolio/${id}`, {
+        const response = await fetch(`https://portfolio-backend-4kbf.onrender.com/api/portfolio/${id}`, {
             method: "DELETE",
             headers: { 
                 "Authorization" : `Bearer ${token}`,
@@ -65,7 +65,7 @@ async function deleteProject(id) {
 
 async function postAdmin(email, password) {
     try{
-        const response = await fetch("http://localhost:4000/api/auth/login", {
+        const response = await fetch("https://portfolio-backend-4kbf.onrender.com/api/auth/login", {
             method: "POST",
             headers: {"Content-Type" : "application/json"},
             body: JSON.stringify({ email, password })
@@ -79,10 +79,4 @@ async function postAdmin(email, password) {
     } catch (error) {
         showMessage(error.message);
     }
-}
-
-
-
-async function getCategories() {
-    const responseCategories = await fetch("http://localhost:4000/")
 }
